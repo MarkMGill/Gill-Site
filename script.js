14302439c5fc22f1c1310080003a2509971704f1
MarkMGill <40337791+MarkMGill@users.noreply.github.com>
2019-08-16 08:00:17 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -26,6 +26,25 @@
             }
         });
 
+        $('#home-section a').on('click', function(e) {
+            //check for a hash value
+            if(this.hash !== '') {
+                //prevent default behavior
+                e.preventDefault();
+  
+                //store hash
+                const hash = this.hash;
+  
+                //animate smooth scroll
+                $('html, body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 900, function() {
+                    //add hash to url after scroll
+                    window.location.hash = hash;
+                });
+              }
+          });
+
 /* Animations on scroll */
 
     $('.js__wp_1').waypoint(function(direction) {
@@ -48,6 +67,29 @@
 
     $('.js__wp_4').waypoint(function(direction) {
             $('.js__wp_4').addClass('animated slow bounceInRight delay-2s faster');
+        }, {
+            offset: '50%'
+        });
+
+    $('.js__wp_5').waypoint(function(direction) {
+            $('.js__wp_5').addClass('animated slow bounceInLeft slower delay-1s');
+        }, {
+            offset: '50%'
+        });
+    $('.js__wp_6').waypoint(function(direction) {
+            $('.js__wp_6').addClass('animated slow bounceInRight');
+        }, {
+            offset: '50%'
+        });
+
+    $('.js__wp_7').waypoint(function(direction) {
+            $('.js__wp_7').addClass('animated slow bounceInRight delay-1s fast');
+        }, {
+            offset: '50%'
+        });
+
+    $('.js__wp_8').waypoint(function(direction) {
+            $('.js__wp_8').addClass('animated slow bounceInRight delay-2s faster');
         }, {
             offset: '50%'
         });
